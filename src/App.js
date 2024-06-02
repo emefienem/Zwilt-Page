2bddc4147f2a75d07118d454b83cc3e0e5d87065
emefienem <emefienemmichael@gmail.com>
2024-06-02 12:08:11 -0700

    push

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Animation from "./components/Animation";
 function App() {
   return (
     <>
-      <div className="section p-8">
+      <div className="section p-8 z-[-1]">
         <Animation animation="fade-in-left">
           <Navbar />
         </Animation>
