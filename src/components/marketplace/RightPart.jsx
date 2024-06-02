ef9ad9b8758aa66fe347137c73a2ac13cc7eb43e
emefienem <emefienemmichael@gmail.com>
2024-06-02 16:38:20 -0700

    push

--- src/components/marketplace/RightPart.jsx
+++ src/components/marketplace/RightPart.jsx
@@ -4,11 +4,11 @@ const RightPart = ({ markets }) => {
   return (
     <div className="bg-white rounded-lg p-7">
       <p className="mb-5 font-semibold">{markets.title}</p>
-      <div className="flex items-start w-full overflow-x-auto md:overflow-x-hidden scrollbar-hide">
+      <div className="flex items-start gap-1 w-full overflow-x-auto md:overflow-x-hidden scrollbar-hide">
         {markets.markets.map((market, index) => (
           <div
             key={index}
-            className="flex-shrink-0 flex flex-col flex-wrap items-center min-w-[150px] sm:min-w-[140px]"
+            className="flex-shrink-0 flex flex-col flex-wrap gap-1 items-center min-w-[150px] sm:min-w-[140px]"
           >
             <div className="w-[76px] h-[76px] lg:w-[86px] lg:h-[86px] rounded-full flex items-center justify-center bg-[#F6F6F6] hover:scale-125 cursor-pointer">
               <img src={market.icon} alt={market.text} />
