d471931da73bea6f80649ae184870d212a7e5955
emefienem <emefienemmichael@gmail.com>
2024-06-02 15:24:16 -0700

    changes

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -51,6 +51,15 @@
 // };
 
 // export default Faq;
+import React, { useState } from "react";
+import YellowLogo from "../assets/yellow-logo.svg";
+
+const faqsQuestion = [
+  "I want to work part-time, is that possible",
+  "How long are the average projects?",
+  "How does the payment works?",
+  "How much can I earn?",
+];
 
 const Faq = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
