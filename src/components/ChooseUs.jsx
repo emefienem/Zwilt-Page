e3169fdfed2d70f38b7eba3cc2f5b1511265d8c1
emefienem <emefienemmichael@gmail.com>
2024-06-03 13:28:58 -0700

    final

--- src/components/ChooseUs.jsx
+++ src/components/ChooseUs.jsx
@@ -168,7 +168,7 @@ const ChooseUs = () => {
   return (
     <section className="gradient-bg text-[#202229] md:min-h-screen slanted-border lg:flex py-20">
       <div className="section px-8 flex flex-col lg:flex-row lg:mt-0 w-full overflow-hidden">
-        <div className="flex-1 lg:w-1/2 flex flex-col justify-center lg:pr-8">
+        <div className="flex-1 lg:w-1/2 flex flex-col justify-center lg:pr-6">
           <div className="max-w-[380px] mb-8 lg:mb-0">
             <h1 className="md:text-5xl text-3xl font-bold mb-8">
               Why choose Zwilt?
@@ -185,10 +185,10 @@ const ChooseUs = () => {
               {ChooseUsData.map((choose, index) => (
                 <div
                   key={index}
-                  className="flex-none p-4 lg:p-10 rounded-[20px] bg-white flex flex-col lg:flex-row items-center justify-between shadow-lg"
+                  className="flex-none p-4 lg:p-6 rounded-[20px] bg-white flex flex-col lg:flex-row items-center justify-between shadow-lg"
                   style={{ minWidth: "100%", maxWidth: "100%" }}
                 >
-                  <div className="lg:max-w-[450px] w-full mb-4 lg:mb-0 lg:pr-8">
+                  <div className="lg:max-w-[450px] w-full mb-4 lg:mb-0">
                     <p className="md:text-4xl text-xl font-bold mb-8">
                       {choose.title}
                     </p>
@@ -219,7 +219,7 @@ const ChooseUs = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="flex-shrink-0">
+                  <div className="flex-shrink-0 lg:w-[40%] lg:pl-6">
                     <img
                       src={choose.image}
                       alt={choose.title}
